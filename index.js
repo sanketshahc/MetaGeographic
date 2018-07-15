9ed58f96fd43fe8f0566c68822af7d17c96a067c
Sanket <sanket@fete.la>
2018-07-15 16:41:50 -0400

    matching logic untested

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 ///////////////////
-
+require('log-timestamp');
 const dotenv = require('dotenv');
 dotenv.config();
 const fs = require('fs');
@@ -11,17 +11,16 @@ const certOptions = {
 
 const express = require('express')
 const app = express()
+const static = express.static;
+
 const https = require('https').createServer(certOptions, app, console.log('up'))
 const socketIO = require('socket.io')(https);
-
 const parse = require('body-parser');
 const expressHBS = require('express-handlebars');
-const static = express.static;
 const users = require('./db');
 const setupAuth = require('./auth');
 const ensureAuthenticated = require('./auth').ensureAuthenticated;
 const sharedSession = require('./auth').sharedSession;
-const logtime = require('log-timestamp');
 
 app.use(static('public'));
 app.use(parse.urlencoded({ extended: false }));
@@ -29,26 +28,6 @@ app.engine('.hbs', expressHBS({defaultLayout: 'layout', extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
 setupAuth(app);
-
-socketIO.on("connection", (socket)=> {
-    // console.log('check it',socket);
-    // console.log('check the handshake stuff \n#1 cookies\n',socket.handshake.cookies, '\n#2 socket conn/client id\n',"'"+socket.client.id+"'", '\n#3 sessionID:\n',"'"+socket.handshake.sessionID+"'");
-    console.log('#2 socket conn/client id\n',"'"+socket.client.id+"'");
-    console.log('#4 all sockets',Object.keys(socketIO.sockets.sockets));
-    // Accept a login event with user's data
-    // socket.on("login", (userdata) => {
-    //     socket.handshake.session.userdata = userdata;
-    //     socket.handshake.session.save();
-    // });
-    // socket.on("logout", (userdata)=> {
-    //     if (socket.handshake.session.userdata) {
-    //         delete socket.handshake.session.userdata;
-    //         socket.handshake.session.save();
-    //     }
-    // });        
-});
-
-
 https.listen(443);
 
 //### eventually export routes from a routes.js file
@@ -61,7 +40,7 @@ let reqFbid;
 
 app.use(
     ensureAuthenticated, 
-    (req,res,next)=>{
+    (req,res,next)=>{ 
         console.log('reqUserSesh:', req.sessionID, 'reqFbid:', req.user);
         reqUserSesh = req.sessionID;
         reqFbid = req.user;
@@ -98,7 +77,6 @@ app.post('/main', (req,res,next)=>{
         .catch(console.log)
     })  
 
-
 app.get('/main', (req,res,next)=>{
     users.allLoggedin()
         .then(element=>{res.send(element)})
@@ -109,3 +87,102 @@ app.get('/*',(req,res,next)=>{
     })
 
 //###
+
+// get all sockets with the io.sockets.sockets
+// and specific socket by io.sockets.sockets['targetSocketID']
+
+// getting all rooms returns the socket's synonymous room, so not useful
+//     console.log('arrAllRooms\n', socketIO.sockets.adapter.rooms)
+
+//arrAllRooms 
+//{ '8_djclj2r1aQx8xLAAAC':
+//     Room {sockets: 
+//             {'8_djclj2r1aQx8xLAAAC': true,
+//              'xxxxxx': true
+//             }, 
+//           length: 1 
+//          } 
+//}
+//###
+
+// callback should be generic to follow 'connection', 'next', and 'disconnect' events
+socketIO.on("connection", (socket)=> {
+    // console.log('check the handshake stuff \n#1 cookies\n',socket.handshake.cookies, '\n#2 socket conn/client id\n',"'"+socket.client.id+"'", '\n#3 sessionID:\n',"'"+socket.handshake.sessionID+"'");
+    console.log('#2 socket conn/client id\n',"'"+socket.client.id+"'");
+    console.log('#3 rooms with socket:', socket.rooms )
+    console.log('#4 all rooms:', socketIO.sockets.adapter.rooms)
+    console.log('#5 all socket-id\'s',Object.keys(socketIO.sockets.sockets));
+    console.log('Did it really work?', socket.handshake.user)
+
+    function log() {
+        var array = ['Message from server:'];
+        array.push.apply(array, arguments);
+        socket.emit('log', array);
+      }
+
+    socket.on('message', function(message) {
+        log('Client said: ', message);
+        // must evaluate room everytime, rather than pass name from function to function.
+        // if must pass name, then nest this function inside the join/create listener.
+        let currentRoomName = socket._rooms[0]
+        socket.to(currentRoomName).emit('message', message);
+    });
+
+    socket.on('create or join', ()=>{
+        let arrWrtcRoomNames = [];
+        // const findRoom = () => {
+        var room;
+        var openRoom = arrWrtcRoomNames.find(
+            (name)=>{
+                var clientsInRoom = socketIO.sockets.adapter.rooms[name];
+                var numClients = clientsInRoom ? Object.keys(clientsInRoom.sockets).length : 0;
+                if (numClients === 1) {
+                    joinRoomToFill(name);
+                    return name
+                } else if (numClients === 0){
+                    joinRoomToWait(name);
+                    return name
+                }
+            }
+        )
+        if (!openRoom) {
+            let newRoom = createRoomToWait();
+            openRoom = newRoom;
+            }
+        room = openRoom;
+        // }
+    
+        const joinRoomToFill = (name) => {
+            log('Client ID ' + socket.id + ' joined room ' + name);
+            socketIO.sockets.in(name).emit('join', name);
+            socket.join(name);
+            socket.emit('joined', name, socket.id);
+            // socketIO.sockets.in(room).emit('ready');
+        }
+    
+        const joinRoomToWait = (name) => {
+            socket.join(name);
+            log('Client ID ' + socket.id + ' created room ' + name);
+            socket.emit('created', name, socket.id);
+        }
+        const createRoomToWait = () => {
+            let newName = nameRoom();
+            socket.join(newRoom);
+            log('Client ID ' + socket.id + ' created room ' + newName);
+            arrWrtcRoomNames.push(newName);
+            socket.emit('created', newRoom, socket.id);
+            return newName
+        }
+         // a helper function to generate room names
+         const nameRoom = () => {
+            let suffix = (arrWrtcRoomNames.length + 1).toString();
+            let prefix = 'room';
+            return roomName = prefix + suffix; 
+        };
+    })
+    
+});
+
+
+// on disconnect, the socket left in the room should advance to next available...
+socketIO.on('disconnect', ()=>{})
